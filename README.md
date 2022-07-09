352b20a06398e67796b48604a2e34f6e9ec0a20b
vrecek <oktawiusz2002@gmail.com>
2022-07-09 20:40:32 +0200

    Image path

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Holdem Poker game
-made in Typescript <br> <br>
+made in Typescript <br><br>
+
 But instead of drawing 3 cards in the first draw, cards are being drawn one by one up to 5
--- ts/Card.ts
+++ ts/Card.ts
@@ -17,94 +17,94 @@ interface Images {
 
 const imageSources: Images[] = [
    {
-      clubs: '../images/2_of_clubs.png',
-      spades: '../images/2_of_spades.png',
-      diamonds: '../images/2_of_diamonds.png',
-      hearts: '../images/2_of_hearts.png'
+      clubs: './../images/2_of_clubs.png',
+      spades: './../images/2_of_spades.png',
+      diamonds: './../images/2_of_diamonds.png',
+      hearts: './../images/2_of_hearts.png'
    },
 
    {
-      clubs: '../images/3_of_clubs.png',
-      spades: '../images/3_of_spades.png',
-      diamonds: '../images/3_of_diamonds.png',
-      hearts: '../images/3_of_hearts.png'
+      clubs: './../images/3_of_clubs.png',
+      spades: './../images/3_of_spades.png',
+      diamonds: './../images/3_of_diamonds.png',
+      hearts: './../images/3_of_hearts.png'
    },
 
    {
-      clubs: '../images/4_of_clubs.png',
-      spades: '../images/4_of_spades.png',
-      diamonds: '../images/4_of_diamonds.png',
-      hearts: '../images/4_of_hearts.png'
+      clubs: './../images/4_of_clubs.png',
+      spades: './../images/4_of_spades.png',
+      diamonds: './../images/4_of_diamonds.png',
+      hearts: './../images/4_of_hearts.png'
    },
 
    {
-      clubs: '../images/5_of_clubs.png',
-      spades: '../images/5_of_spades.png',
-      diamonds: '../images/5_of_diamonds.png',
-      hearts: '../images/5_of_hearts.png'
+      clubs: './../images/5_of_clubs.png',
+      spades: './../images/5_of_spades.png',
+      diamonds: './../images/5_of_diamonds.png',
+      hearts: './../images/5_of_hearts.png'
    },
 
    {
-      clubs: '../images/6_of_clubs.png',
-      spades: '../images/6_of_spades.png',
-      diamonds: '../images/6_of_diamonds.png',
-      hearts: '../images/6_of_hearts.png'
+      clubs: './../images/6_of_clubs.png',
+      spades: './../images/6_of_spades.png',
+      diamonds: './../images/6_of_diamonds.png',
+      hearts: './../images/6_of_hearts.png'
    },
 
    {
-      clubs: '../images/7_of_clubs.png',
-      spades: '../images/7_of_spades.png',
-      diamonds: '../images/7_of_diamonds.png',
-      hearts: '../images/7_of_hearts.png'
+      clubs: './../images/7_of_clubs.png',
+      spades: './../images/7_of_spades.png',
+      diamonds: './../images/7_of_diamonds.png',
+      hearts: './../images/7_of_hearts.png'
    },
 
    {
-      clubs: '../images/8_of_clubs.png',
-      spades: '../images/8_of_spades.png',
-      diamonds: '../images/8_of_diamonds.png',
-      hearts: '../images/8_of_hearts.png'
+      clubs: './../images/8_of_clubs.png',
+      spades: './../images/8_of_spades.png',
+      diamonds: './../images/8_of_diamonds.png',
+      hearts: './../images/8_of_hearts.png'
    },
 
    {
-      clubs: '../images/9_of_clubs.png',
-      spades: '../images/9_of_spades.png',
-      diamonds: '../images/9_of_diamonds.png',
-      hearts: '../images/9_of_hearts.png'
+      clubs: './../images/9_of_clubs.png',
+      spades: './../images/9_of_spades.png',
+      diamonds: './../images/9_of_diamonds.png',
+      hearts: './../images/9_of_hearts.png'
    },
 
    {
-      clubs: '../images/10_of_clubs.png',
-      spades: '../images/10_of_spades.png',
-      diamonds: '../images/10_of_diamonds.png',
-      hearts: '../images/10_of_hearts.png'
+      clubs: './../images/10_of_clubs.png',
+      spades: './../images/10_of_spades.png',
+      diamonds: './../images/10_of_diamonds.png',
+      hearts: './../images/10_of_hearts.png'
    },
 
    {
-      clubs: '../images/jack_of_clubs2.png',
-      spades: '../images/jack_of_spades2.png',
-      diamonds: '../images/jack_of_diamonds2.png',
-      hearts: '../images/jack_of_hearts2.png'
+      clubs: './../images/jack_of_clubs2.png',
+      spades: './../images/jack_of_spades2.png',
+      diamonds: './../images/jack_of_diamonds2.png',
+      hearts: './../images/jack_of_hearts2.png'
    },
 
    {
-      clubs: '../images/queen_of_clubs2.png',
-      spades: '../images/queen_of_spades2.png',
-      diamonds: '../images/queen_of_diamonds2.png',
-      hearts: '../images/queen_of_hearts2.png'
+      clubs: './../images/queen_of_clubs2.png',
+      spades: './../images/queen_of_spades2.png',
+      diamonds: './../images/queen_of_diamonds2.png',
+      hearts: './../images/queen_of_hearts2.png'
    },
 
    {
-      clubs: '../images/king_of_clubs2.png',
-      spades: '../images/king_of_spades2.png',
-      diamonds: '../images/king_of_diamonds2.png',
-      hearts: '../images/king_of_hearts2.png'
+      clubs: './../images/king_of_clubs2.png',
+      spades: './../images/king_of_spades2.png',
+      diamonds: './../images/king_of_diamonds2.png',
+      hearts: './../images/king_of_hearts2.png'
    },
 
    {
-      clubs: '../images/ace_of_clubs.png',
-      spades: '../images/ace_of_spades.png',
-      diamonds: '../images/ace_of_diamonds.png',
-      hearts: '../images/ace_of_hearts.png'
+      clubs: './../images/ace_of_clubs.png',
+      spades: './../images/ace_of_spades.png',
+      diamonds: './../images/ace_of_diamonds.png',
+      hearts: './../images/ace_of_hearts.png'
    }
 ]
 
@@ -201,7 +201,7 @@ export default class Card {
    public appendCard(container: HTMLElement, hideCard?: boolean): void {
       const img = document.createElement('img')
 
-      img.src = hideCard ? '../images/back.png' : this.image
+      img.src = hideCard ? './.././../images/back.png' : this.image
 
       container.appendChild(img)
    }
